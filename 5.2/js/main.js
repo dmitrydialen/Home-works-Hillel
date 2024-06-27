012f20af5c07115ed7d7a44aef7a423563b08c5c
Uvarov Dmitry <dimauvarov04031993@gmail.com>
2024-06-28 01:02:25 +0300

    Update dz 5.2

--- 5.2/js/main.js
+++ 5.2/js/main.js
@@ -1,8 +1,10 @@
 // Один долар коштує 26 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів
 
 const exchangeRate = 26;
+const maxRate = 100;
+const rateStep = 10;
 
-for (let dollars = 10; dollars <= 100; dollars += 10) {
+for (let dollars = 10; dollars <= maxRate; dollars += rateStep) {
     let hryvnias = dollars * exchangeRate;
     console.log(`${dollars} $ = ${hryvnias} UAH`);
 }
