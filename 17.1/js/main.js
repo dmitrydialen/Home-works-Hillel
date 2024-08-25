4abba3c88002e5a6f7405f974e95d8ae2f4ad9e5
Dmitry Dialen <104232574+dmitrydialen@users.noreply.github.com>
2024-08-25 19:08:03 +0300

    Add work 17.1

---
+++ 17.1/js/main.js
@@ -0,0 +1,28 @@
+class Calculator {
+    add(a, b) {
+      return a + b;
+    }
+  
+    subtract(a, b) {
+      return a - b;
+    }
+  
+    multiply(a, b) {
+      return a * b;
+    }
+  
+    divide(a, b) {
+      if (b === 0) {
+        throw new Error("Деление на ноль невозможно");
+      }
+      return a / b;
+    }
+  }
+  
+  const calc = new Calculator();
+  
+  console.log(calc.add(5, 3));       
+  console.log(calc.subtract(10, 4));  
+  console.log(calc.multiply(3, 6));   
+  console.log(calc.divide(8, 2));
+  
