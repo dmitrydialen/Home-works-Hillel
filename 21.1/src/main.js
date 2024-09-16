b4ed06ffa4e6a294d8f11d4b1075e6f1c0c4d31a
Dmitry Dialen <104232574+dmitrydialen@users.noreply.github.com>
2024-09-16 13:06:33 +0300

    Add work 21.1

---
+++ 21.1/src/main.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'vertical',
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+
+
+  var map = L.map('map').setView([50.4501, 30.5234], 13);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
